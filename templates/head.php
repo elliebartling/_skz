1b44696318733974deee309bf79b25474936797b
Ellen Bartling <ellenbartling@gmail.com>
2017-07-30 17:32:52 -0500

    Add google adsense scripts

--- templates/head.php
+++ templates/head.php
@@ -3,4 +3,11 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+      google_ad_client: "ca-pub-8818637726316507",
+      enable_page_level_ads: true
+    });
+  </script>
 </head>
